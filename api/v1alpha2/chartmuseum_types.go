fa8f43db9f431bf0ac1404c10603d60346a36b32
Pierre Péronnet <pierre.peronnet@ovhcloud.com>
2020-04-02 13:22:37 +0200

    feat(crd) Add goharbor category to crd
    Signed-off-by: Pierre Péronnet <pierre.peronnet@ovhcloud.com>

--- api/v1alpha2/chartmuseum_types.go
+++ api/v1alpha2/chartmuseum_types.go
@@ -26,6 +26,7 @@ const (
 // +k8s:openapi-gen=true
 // +resource:path=chartmuseum
 // +kubebuilder:subresource:status
+// +kubebuilder:resource:categories="goharbor"
 // +kubebuilder:printcolumn:name="Version",type=string,JSONPath=`.spec.version`,description="The semver Harbor version",priority=5
 // +kubebuilder:printcolumn:name="Replicas",type=string,JSONPath=`.spec.replicas`,description="The number of replicas",priority=0
 type ChartMuseum struct {
