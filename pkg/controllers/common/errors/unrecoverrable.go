5122c607f5731dae70759f5891779b7a3a429950
Pierre Péronnet <pierre.peronnet@ovhcloud.com>
2020-04-14 16:17:20 +0200

    wip
    Signed-off-by: Pierre Péronnet <pierre.peronnet@ovhcloud.com>

--- pkg/controllers/common/errors/unrecoverrable.go
+++ pkg/controllers/common/errors/unrecoverrable.go
@@ -1,26 +1,50 @@
 package errors
 
 import (
+	"github.com/pkg/errors"
 	v1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/kustomize/kstatus/status"
 )
 
-type UnrecoverrableError struct {
-	Cause error
+const (
+	OperatorReason = "operatorError"
+)
 
-	Reason  string
-	Message string
+type unrecoverrableError struct {
+	cause error
+
+	reason  string
+	message string
 }
 
-func (err UnrecoverrableError) Error() string {
-	return err.Cause.Error()
-}
-
-func (err UnrecoverrableError) Status() status.Condition {
-	return status.Condition{
-		Type:    status.ConditionFailed,
-		Message: err.Message,
-		Reason:  err.Message,
-		Status:  v1.ConditionTrue,
+func UnrecoverrableError(err error, reason, message string) error {
+	return &unrecoverrableError{
+		cause:   err,
+		reason:  reason,
+		message: message,
 	}
+}
+
+func (err *unrecoverrableError) Error() string {
+	return errors.Wrap(err.cause, err.message).Error()
+}
+
+func (err *unrecoverrableError) Status() []status.Condition {
+	return []status.Condition{
+		{
+			Type:    status.ConditionInProgress,
+			Message: "",
+			Reason:  "",
+			Status:  v1.ConditionFalse,
+		}, {
+			Type:    status.ConditionFailed,
+			Message: err.cause.Error(),
+			Reason:  err.reason,
+			Status:  v1.ConditionTrue,
+		},
+	}
+}
+
+func (err *unrecoverrableError) Cause() error {
+	return err.cause
 }
