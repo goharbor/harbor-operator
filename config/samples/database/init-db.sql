bd1365f5672911197b10d5d0a98312707c28506f
Pierre Péronnet <pierre.peronnet@ovhcloud.com>
2020-07-06 11:50:48 +0200

    feat(sample) Use a single database for all components feat(sample) Add harbor sample
    Signed-off-by: Pierre Péronnet <pierre.peronnet@ovhcloud.com>

---
+++ config/samples/database/init-db.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE core WITH OWNER postgres;
+CREATE DATABASE clair WITH OWNER postgres;
+CREATE DATABASE notary-server WITH OWNER postgres;
+CREATE DATABASE notary-signer WITH OWNER postgres;
