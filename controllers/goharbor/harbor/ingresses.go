6bec20431058f49107955b914bfa1dec271310f3
Pierre Péronnet <pierre.peronnet@ovhcloud.com>
2020-07-06 14:55:05 +0200

    fix(ingress) Ingresses use right backend
    Signed-off-by: Pierre Péronnet <pierre.peronnet@ovhcloud.com>

--- controllers/goharbor/harbor/ingresses.go
+++ controllers/goharbor/harbor/ingresses.go
@@ -16,7 +16,21 @@ import (
 	"github.com/goharbor/harbor-operator/controllers/goharbor/notaryserver"
 	"github.com/goharbor/harbor-operator/controllers/goharbor/portal"
 	"github.com/goharbor/harbor-operator/controllers/goharbor/registry"
+	"github.com/goharbor/harbor-operator/pkg/graph"
 )
+
+type CoreIngress graph.Resource
+
+func (r *Reconciler) AddCoreIngress(ctx context.Context, harbor *goharborv1alpha2.Harbor, core Core, portal Portal, registry Registry) (CoreIngress, error) {
+	ingress, err := r.GetCoreIngresse(ctx, harbor)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot get core ingress")
+	}
+
+	ingressRes, err := r.Controller.AddIngressToManage(ctx, ingress, core, portal, registry)
+
+	return CoreIngress(ingressRes), errors.Wrap(err, "cannot add core ingress")
+}
 
 func getHostAndIngresses(harbor *goharborv1alpha2.Harbor) (string, []netv1.IngressTLS, error) {
 	u, err := url.Parse(harbor.Spec.ExternalURL)
@@ -37,43 +51,47 @@ func getHostAndIngresses(harbor *goharborv1alpha2.Harbor) (string, []netv1.Ingre
 }
 
 func (r *Reconciler) GetCoreIngresse(ctx context.Context, harbor *goharborv1alpha2.Harbor) (*netv1.Ingress, error) {
+	if harbor.Spec.Expose.Ingress == nil {
+		return nil, nil
+	}
+
 	host, tls, err := getHostAndIngresses(harbor)
 	if err != nil {
 		return nil, errors.Wrap(err, "cannot get host and ingresses")
 	}
 
 	coreBackend := netv1.IngressBackend{
-		ServiceName: harbor.NormalizeComponentName(goharborv1alpha2.CoreName),
+		ServiceName: r.NormalizeName(ctx, harbor.GetName(), "core"),
 		ServicePort: intstr.FromInt(core.PublicPort),
 	}
-
-	rules := []netv1.HTTPIngressPath{
-		{
-			Path:    "/api",
-			Backend: coreBackend,
-		}, {
-			Path:    "/c",
-			Backend: coreBackend,
-		}, {
-			Path:    "/chartrepo",
-			Backend: coreBackend,
-		}, {
-			Path:    "/service",
-			Backend: coreBackend,
-		}, {
-			Path: "/",
-			Backend: netv1.IngressBackend{
-				ServiceName: harbor.NormalizeComponentName(goharborv1alpha2.PortalName),
-				ServicePort: intstr.FromInt(portal.PublicPort),
-			},
-		}, {
-			Path: "/v2",
-			Backend: netv1.IngressBackend{
-				ServiceName: harbor.NormalizeComponentName(goharborv1alpha2.RegistryName),
-				ServicePort: intstr.FromInt(registry.PublicPort),
-			},
-		},
+	portalBackend := netv1.IngressBackend{
+		ServiceName: r.NormalizeName(ctx, harbor.GetName(), "portal"),
+		ServicePort: intstr.FromInt(portal.PublicPort),
 	}
+	registryBackend := netv1.IngressBackend{
+		ServiceName: r.NormalizeName(ctx, harbor.GetName(), "registry"),
+		ServicePort: intstr.FromInt(registry.PublicPort),
+	}
+
+	rules := []netv1.HTTPIngressPath{{
+		Path:    "/api",
+		Backend: coreBackend,
+	}, {
+		Path:    "/c",
+		Backend: coreBackend,
+	}, {
+		Path:    "/chartrepo",
+		Backend: coreBackend,
+	}, {
+		Path:    "/service",
+		Backend: coreBackend,
+	}, {
+		Path:    "/",
+		Backend: portalBackend,
+	}, {
+		Path:    "/v2",
+		Backend: registryBackend,
+	}}
 
 	return &netv1.Ingress{
 		ObjectMeta: metav1.ObjectMeta{
@@ -82,39 +100,50 @@ func (r *Reconciler) GetCoreIngresse(ctx context.Context, harbor *goharborv1alph
 		},
 		Spec: netv1.IngressSpec{
 			TLS: tls,
-			Rules: []netv1.IngressRule{
-				{
-					Host: host,
-					IngressRuleValue: netv1.IngressRuleValue{
-						HTTP: &netv1.HTTPIngressRuleValue{
-							Paths: rules,
-						},
+			Rules: []netv1.IngressRule{{
+				Host: host,
+				IngressRuleValue: netv1.IngressRuleValue{
+					HTTP: &netv1.HTTPIngressRuleValue{
+						Paths: rules,
 					},
 				},
-			},
+			}},
 		},
 	}, nil
 }
 
+type NotaryIngress graph.Resource
+
+func (r *Reconciler) AddNotaryIngress(ctx context.Context, harbor *goharborv1alpha2.Harbor, notary NotaryServer) (NotaryIngress, error) {
+	ingress, err := r.GetNotaryServerIngresse(ctx, harbor)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot get notary ingress")
+	}
+
+	ingressRes, err := r.Controller.AddIngressToManage(ctx, ingress, notary)
+
+	return NotaryIngress(ingressRes), errors.Wrapf(err, "cannot add notary ingress")
+}
+
 func (r *Reconciler) GetNotaryServerIngresse(ctx context.Context, harbor *goharborv1alpha2.Harbor) (*netv1.Ingress, error) {
-	if harbor.Spec.Notary != nil {
+	if harbor.Spec.Notary == nil {
+		return nil, nil
+	}
+
+	if harbor.Spec.Expose.Ingress == nil {
 		return nil, nil
 	}
 
 	u, err := url.Parse(harbor.Spec.Expose.Ingress.Hosts.Notary)
 	if err != nil {
-		panic(errors.Wrap(err, "invalid url"))
+		return nil, errors.Wrap(err, "invalid url")
 	}
-
-	notaryHost := strings.SplitN(u.Host, ":", 1)
 
 	var tls []netv1.IngressTLS
 	if u.Scheme == "https" {
-		tls = []netv1.IngressTLS{
-			{
-				SecretName: harbor.Spec.Expose.TLS.CertificateRef,
-			},
-		}
+		tls = []netv1.IngressTLS{{
+			SecretName: harbor.Spec.Expose.TLS.CertificateRef,
+		}}
 	}
 
 	return &netv1.Ingress{
@@ -124,24 +153,20 @@ func (r *Reconciler) GetNotaryServerIngresse(ctx context.Context, harbor *goharb
 		},
 		Spec: netv1.IngressSpec{
 			TLS: tls,
-			Rules: []netv1.IngressRule{
-				{
-					Host: notaryHost[0],
-					IngressRuleValue: netv1.IngressRuleValue{
-						HTTP: &netv1.HTTPIngressRuleValue{
-							Paths: []netv1.HTTPIngressPath{
-								{
-									Path: "/",
-									Backend: netv1.IngressBackend{
-										ServiceName: harbor.NormalizeComponentName(goharborv1alpha2.NotaryServerName),
-										ServicePort: intstr.FromInt(notaryserver.PublicPort),
-									},
-								},
+			Rules: []netv1.IngressRule{{
+				Host: u.Hostname(),
+				IngressRuleValue: netv1.IngressRuleValue{
+					HTTP: &netv1.HTTPIngressRuleValue{
+						Paths: []netv1.HTTPIngressPath{{
+							Path: "/",
+							Backend: netv1.IngressBackend{
+								ServiceName: r.NormalizeName(ctx, harbor.GetName(), "notary-server"),
+								ServicePort: intstr.FromInt(notaryserver.PublicPort),
 							},
-						},
+						}},
 					},
 				},
-			},
+			}},
 		},
 	}, nil
 }
