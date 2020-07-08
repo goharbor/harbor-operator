19e405c7129af41ee305f490b5f51564801f1cc7
Antoine Blondeau <antoine@blondeau.me>
2020-07-08 10:44:02 +0200

    Add trivy deployment
    Signed-off-by: Antoine Blondeau <antoine@blondeau.me>

---
+++ controllers/goharbor/trivy/resources.go
@@ -0,0 +1,64 @@
+package trivy
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+
+	goharborv1alpha2 "github.com/goharbor/harbor-operator/apis/goharbor.io/v1alpha2"
+	serrors "github.com/goharbor/harbor-operator/pkg/controller/errors"
+	"github.com/goharbor/harbor-operator/pkg/resources"
+)
+
+func (r *Reconciler) NewEmpty(_ context.Context) resources.Resource {
+	return &goharborv1alpha2.Trivy{}
+}
+
+func (r *Reconciler) AddResources(ctx context.Context, resource resources.Resource) error {
+	trivy, ok := resource.(*goharborv1alpha2.Trivy)
+	if !ok {
+		return serrors.UnrecoverrableError(errors.Errorf("%+v", resource), serrors.OperatorReason, "unable to add resource")
+	}
+
+	service, err := r.GetService(ctx, trivy)
+	if err != nil {
+		return errors.Wrap(err, "cannot get service")
+	}
+
+	_, err = r.Controller.AddServiceToManage(ctx, service)
+	if err != nil {
+		return errors.Wrapf(err, "cannot add service %s", service.GetName())
+	}
+
+	configMap, err := r.GetConfigMap(ctx, trivy)
+	if err != nil {
+		return errors.Wrap(err, "cannot get configMap")
+	}
+
+	configMapResource, err := r.Controller.AddConfigMapToManage(ctx, configMap)
+	if err != nil {
+		return errors.Wrapf(err, "cannot add configMap %s", configMap.GetName())
+	}
+
+	secret, err := r.GetSecret(ctx, trivy)
+	if err != nil {
+		return errors.Wrap(err, "cannot get configMap")
+	}
+
+	secretResource, err := r.Controller.AddSecretToManage(ctx, secret)
+	if err != nil {
+		return errors.Wrapf(err, "cannot add configMap %s", configMap.GetName())
+	}
+
+	deployment, err := r.GetDeployment(ctx, trivy)
+	if err != nil {
+		return errors.Wrap(err, "cannot get deployment")
+	}
+
+	_, err = r.Controller.AddDeploymentToManage(ctx, deployment, configMapResource, secretResource)
+	if err != nil {
+		return errors.Wrapf(err, "cannot add deployment %s", deployment.GetName())
+	}
+
+	return nil
+}
