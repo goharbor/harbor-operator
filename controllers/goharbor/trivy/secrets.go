e0563854ec975638e8dbec26cc649e19bdffd19e
Pierrick Gicquelais <pierrick.gicquelais@epitech.eu>
2020-07-22 15:41:42 +0200

    feat(trivy): Enable Trivy deployment on harbor-operator
    Signed-off-by: Pierrick Gicquelais <pierrick.gicquelais@epitech.eu>

--- controllers/goharbor/trivy/secrets.go
+++ controllers/goharbor/trivy/secrets.go
@@ -8,13 +8,31 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 
 	goharborv1alpha2 "github.com/goharbor/harbor-operator/apis/goharbor.io/v1alpha2"
+	harbormetav1 "github.com/goharbor/harbor-operator/apis/meta/v1alpha1"
 	"github.com/pkg/errors"
 )
 
+func (r *Reconciler) AddSecret(ctx context.Context, trivy *goharborv1alpha2.Trivy) error {
+	// Forge the secret resource
+	secret, err := r.GetSecret(ctx, trivy)
+	if err != nil {
+		return errors.Wrap(err, "cannot get secret")
+	}
+
+	// Add secret to reconciler controller
+	_, err = r.Controller.AddSecretToManage(ctx, secret)
+	if err != nil {
+		return errors.Wrapf(err, "cannot manage secret %s", secret.GetName())
+	}
+
+	return nil
+}
+
 func (r *Reconciler) GetSecret(ctx context.Context, trivy *goharborv1alpha2.Trivy) (*corev1.Secret, error) {
+	var redisPassword string
+
 	name := r.NormalizeName(ctx, trivy.GetName())
 	namespace := trivy.GetNamespace()
-	var redisPassword string
 
 	if trivy.Spec.Cache.Redis.PasswordRef != "" {
 		var passwordSecret corev1.Secret
@@ -27,18 +45,15 @@ func (r *Reconciler) GetSecret(ctx context.Context, trivy *goharborv1alpha2.Triv
 			return nil, errors.Wrap(err, "cannot get redis password")
 		}
 
-		password, ok := passwordSecret.Data[goharborv1alpha2.RedisPasswordKey]
+		password, ok := passwordSecret.Data[harbormetav1.RedisPasswordKey]
 		if !ok {
-			return nil, errors.Errorf("%s not found in secret %s", goharborv1alpha2.RedisPasswordKey, trivy.Spec.Cache.Redis.PasswordRef)
+			return nil, errors.Errorf("%s not found in secret %s", harbormetav1.RedisPasswordKey, trivy.Spec.Cache.Redis.PasswordRef)
 		}
 
 		redisPassword = string(password)
 	}
 
-	redisDSN, err := trivy.Spec.Cache.Redis.GetDSN(redisPassword)
-	if err != nil {
-		return nil, errors.Wrap(err, "cannot get redis DSN")
-	}
+	redisDSN := trivy.Spec.Cache.Redis.GetDSN(redisPassword)
 
 	return &corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
@@ -46,7 +61,8 @@ func (r *Reconciler) GetSecret(ctx context.Context, trivy *goharborv1alpha2.Triv
 			Namespace: namespace,
 		},
 		StringData: map[string]string{
-			"SCANNER_REDIS_URL": redisDSN.String(),
+			"SCANNER_REDIS_URL":           redisDSN.String(),
+			"SCANNER_JOB_QUEUE_REDIS_URL": redisDSN.String(),
 		},
 	}, nil
 }
