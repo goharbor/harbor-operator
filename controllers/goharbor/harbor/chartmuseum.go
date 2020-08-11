1b1865ab07d07b11c52f69767c018170dadd65d0
Pierre Péronnet <pierre.peronnet@ovhcloud.com>
2020-08-11 15:50:12 +0200

    feat(apis) Normalize *authentication* fields
    Signed-off-by: Pierre Péronnet <pierre.peronnet@ovhcloud.com>

--- controllers/goharbor/harbor/chartmuseum.go
+++ controllers/goharbor/harbor/chartmuseum.go
@@ -90,7 +90,7 @@ func (r *Reconciler) GetChartMuseum(ctx context.Context, harbor *goharborv1alpha
 		},
 		Spec: goharborv1alpha2.ChartMuseumSpec{
 			ComponentSpec: harbor.Spec.ChartMuseum.ComponentSpec,
-			Auth: goharborv1alpha2.ChartMuseumAuthSpec{
+			Authentication: goharborv1alpha2.ChartMuseumAuthSpec{
 				AnonymousGet: false,
 				BasicAuthRef: basicAuthRef,
 			},
