4ff5b0bb89910f591c2bbd6d8034cecf60e0ed0f
Pierre Péronnet <pierre.peronnet@ovhcloud.com>
2020-08-28 11:23:16 +0200

    fix(linter) godot: Add dot to top level comments
    Signed-off-by: Pierre Péronnet <pierre.peronnet@ovhcloud.com>

--- apis/meta/v1alpha1/secrets.go
+++ apis/meta/v1alpha1/secrets.go
@@ -8,68 +8,68 @@ const (
 	// SecretTypeHTPasswd contains data needed for authenticate users against password.
 	//
 	// Required field:
-	// - Secret.Data["auth"] - File containing user:password lines
+	// - Secret.Data["auth"] - File containing user:password lines.
 	SecretTypeHTPasswd corev1.SecretType = "goharbor.io/htpasswd" // nolint:gosec
 
-	// HTPasswdFileName is the file of the users required for SecretTypeHTPasswd secrets
+	// HTPasswdFileName is the file of the users required for SecretTypeHTPasswd secrets.
 	HTPasswdFileName = "htpasswd" // https://kubernetes.github.io/ingress-nginx/examples/auth/basic/#basic-authentication
 )
 
 const (
-	// SecretTypeSharedSecret contains a single secret to share
+	// SecretTypeSharedSecret contains a single secret to share.
 	//
 	// Required field:
-	// - Secret.Data["secret"] - secret to shared
+	// - Secret.Data["secret"] - secret to shared.
 	SecretTypeSingle corev1.SecretType = "goharbor.io/single"
 
-	// SharedSecretKey is the password required for SecretTypeSingle
+	// SharedSecretKey is the password required for SecretTypeSingle.
 	SharedSecretKey = "secret"
 )
 
 const (
-	// SecretTypeRedis contains a password to connect to redis
+	// SecretTypeRedis contains a password to connect to redis.
 	//
 	// Required field:
-	// - Secret.Data["redis-password"] - password to connect to redis, may be empty
+	// - Secret.Data["redis-password"] - password to connect to redis, may be empty.
 	SecretTypeRedis corev1.SecretType = "goharbor.io/redis"
 
-	// RedisPasswordKey is the password to connect to redis
+	// RedisPasswordKey is the password to connect to redis.
 	RedisPasswordKey = "redis-password" // https://github.com/bitnami/charts/blob/master/bitnami/redis/templates/secret.yaml#L14
 )
 
 const (
-	// SecretTypeSharedSecret contains password for a postgresql user
+	// SecretTypeSharedSecret contains password for a postgresql user.
 	//
 	// Required field:
-	// - Secret.Data["redis-password"] - password to connect to redis, may be empty
+	// - Secret.Data["redis-password"] - password to connect to redis, may be empty.
 	SecretTypePostgresql corev1.SecretType = "goharbor.io/postgresql"
 
-	// PostgresqlPasswordKey is the password to connect to postgresql
+	// PostgresqlPasswordKey is the password to connect to postgresql.
 	PostgresqlPasswordKey = "postgresql-password" // nolint:gosec
 )
 
 const (
-	// SecretTypeCSRF contains data needed for CSRF security
+	// SecretTypeCSRF contains data needed for CSRF security.
 	//
 	// Required field:
-	// - Secret.Data["key"] - CSRF key
+	// - Secret.Data["key"] - CSRF key.
 	SecretTypeCSRF corev1.SecretType = "goharbor.io/csrf" // nolint:gosec
 
-	// CSRFSecretKey is the key for SecretTypeCSRF
+	// CSRFSecretKey is the key for SecretTypeCSRF.
 	CSRFSecretKey = "key"
 )
 
 const (
 	// SecretTypeNotarySignerAliases contains aliases for encryption keys.
-	// Only "default" key is required
+	// Only "default" key is required.
 	// Keys must match [a-zA-Z]([a-zA-Z0-9_]*[a-zA-Z0-9])?
-	// Passwords may be any string
+	// Passwords may be any string.
 	//
 	// Required field:
-	// - Secret.Data["default"] - The default password
+	// - Secret.Data["default"] - The default password.
 	SecretTypeNotarySignerAliases corev1.SecretType = "goharbor.io/notary-signer-aliases"
 
-	// SharedSecretKey is the default password to use
+	// SharedSecretKey is the default password to use.
 	DefaultAliasSecretKey = "default"
 )
 
@@ -77,12 +77,12 @@ const (
 	// SecretTypeGithubToken contains credentials to contact Github API.
 	//
 	// Required field:
-	// - Secret.Data["github-token"] - The Github token
+	// - Secret.Data["github-token"] - The Github token.
 	SecretTypeGithubToken corev1.SecretType = "goharbor.io/github" // nolint:gosec
 
-	// GithubTokenPasswordKey is the token to use with the account
+	// GithubTokenPasswordKey is the token to use with the account.
 	GithubTokenPasswordKey = "github-token"
-	// GithubTokenUserKey is the github account to use
+	// GithubTokenUserKey is the github account to use.
 	GithubTokenUserKey = "github-user"
 )
 
@@ -94,6 +94,6 @@ const (
 	// Otherwise, a string starting with yaml special character may break the configuration.
 	SecretTypeRegistry corev1.SecretType = "goharbor.io/registry" // nolint:gosec
 
-	// RegistryHTTPSecret is the http secret
+	// RegistryHTTPSecret is the http secret.
 	RegistryHTTPSecret = "REGISTRY_HTTP_SECRET" // nolint:gosec
 )
