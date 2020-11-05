1a7598a60e642832f82f4cd7e300e03e40a7257a
wangcanfeng <wangcanfeng@corp.netease.com>
2020-11-05 09:57:32 +0800

    feat(harborcluster): update service and lcm interface
    Signed-off-by: wangcanfeng <wangcanfeng@corp.netease.com>

--- controllers/goharbor/harborcluster/database/postgresql.go
+++ controllers/goharbor/harborcluster/database/postgresql.go
@@ -1,13 +1,21 @@
 package database
 
 import (
-	goharborv1alpha2 "github.com/goharbor/harbor-operator/apis/goharbor.io/v1alpha2"
+	"github.com/goharbor/harbor-operator/apis/goharbor.io/v1alpha2"
 	"github.com/goharbor/harbor-operator/pkg/lcm"
 )
 
 type PostgreSQLReconciler struct {
 }
 
-func (p PostgreSQLReconciler) Reconcile(harborCluster *goharborv1alpha2.HarborCluster) (*lcm.CRStatus, error) {
+func (p PostgreSQLReconciler) Apply(harborcluster *v1alpha2.HarborCluster) (*lcm.CRStatus, error) {
+	panic("implement me")
+}
+
+func (p PostgreSQLReconciler) Delete(harborcluster *v1alpha2.HarborCluster) (*lcm.CRStatus, error) {
+	panic("implement me")
+}
+
+func (p PostgreSQLReconciler) Upgrade(harborcluster *v1alpha2.HarborCluster) (*lcm.CRStatus, error) {
 	panic("implement me")
 }
--- controllers/goharbor/harborcluster/storage/minio.go
+++ controllers/goharbor/harborcluster/storage/minio.go
@@ -1,13 +1,21 @@
 package storage
 
 import (
-	goharborv1alpha2 "github.com/goharbor/harbor-operator/apis/goharbor.io/v1alpha2"
+	"github.com/goharbor/harbor-operator/apis/goharbor.io/v1alpha2"
 	"github.com/goharbor/harbor-operator/pkg/lcm"
 )
 
 type MinIOReconciler struct {
 }
 
-func (m MinIOReconciler) Reconcile(harborCluster *goharborv1alpha2.HarborCluster) (*lcm.CRStatus, error) {
+func (m MinIOReconciler) Apply(harborcluster *v1alpha2.HarborCluster) (*lcm.CRStatus, error) {
+	panic("implement me")
+}
+
+func (m MinIOReconciler) Delete(harborcluster *v1alpha2.HarborCluster) (*lcm.CRStatus, error) {
+	panic("implement me")
+}
+
+func (m MinIOReconciler) Upgrade(harborcluster *v1alpha2.HarborCluster) (*lcm.CRStatus, error) {
 	panic("implement me")
 }
