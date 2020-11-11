7709c4987ca687d17c8005a563074c84b1a0248b
wangcanfengxs <wangcanfeng@corp.netease.com>
2020-11-11 16:42:50 +0800

    feat(harborcontroller): implement HarborController. (#186)
    implement Apply method of HarborController
    
    Signed-off-by: wangcanfeng <wangcanfeng@corp.netease.com>

---
+++ controllers/goharbor/harborcluster/harbor/errors.go
@@ -0,0 +1,11 @@
+package harbor
+
+const (
+	GetHarborCRError               = "Get harbor.goharbor.io CR error"
+	CreateHarborCRError            = "Create harbor.goharbor.io CR error"
+	ScaleHarborCRError             = "Scale harbor.goharbor.io CR error"
+	UpdateHarborCRError            = "Update harbor.goharbor.io CR error"
+	EmptyHarborCRStatusError       = "Empty harbor.goharbor.io CR status error"
+	CreateRegistryCertError        = "Create Registry Cert error"
+	AutoGenerateAdminPasswordError = "Auto generate admin password error"
+)
