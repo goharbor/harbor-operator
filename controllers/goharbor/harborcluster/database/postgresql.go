cb8929834e9fcbbc6f94e26093af2500ca208b43
zhuhuijun <zhuhuijunzhj@gmail.com>
2020-11-14 00:00:37 +0800

    feat(options) support ctrl options for lcm controllers
    upport ctrl options for lcm controllers
    
    Signed-off-by: zhuhuijun <zhuhuijunzhj@gmail.com>

--- controllers/goharbor/harborcluster/database/postgresql.go
+++ controllers/goharbor/harborcluster/database/postgresql.go
@@ -1,11 +1,26 @@
 package database
 
 import (
+	"context"
+
+	"github.com/go-logr/logr"
 	"github.com/goharbor/harbor-operator/apis/goharbor.io/v1alpha2"
+	goharborv1alpha2 "github.com/goharbor/harbor-operator/apis/goharbor.io/v1alpha2"
+	"github.com/goharbor/harbor-operator/pkg/k8s"
 	"github.com/goharbor/harbor-operator/pkg/lcm"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 type PostgreSQLController struct {
+	HarborCluster *goharborv1alpha2.HarborCluster
+	Ctx           context.Context
+	Client        k8s.Client
+	Log           logr.Logger
+	DClient       k8s.DClient
+	Scheme        *runtime.Scheme
+	ExpectCR      *unstructured.Unstructured
+	ActualCR      *unstructured.Unstructured
 }
 
 func (p *PostgreSQLController) Apply(harborcluster *v1alpha2.HarborCluster) (*lcm.CRStatus, error) {
@@ -20,6 +35,19 @@ func (p *PostgreSQLController) Upgrade(harborcluster *v1alpha2.HarborCluster) (*
 	panic("implement me")
 }
 
-func NewDatabaseController() lcm.Controller {
-	return &PostgreSQLController{}
+func NewDatabaseController(ctx context.Context, options ...k8s.Option) lcm.Controller {
+
+	o := &k8s.CtrlOptions{}
+
+	for _, option := range options {
+		option(o)
+	}
+
+	return &PostgreSQLController{
+		Ctx:     ctx,
+		Client:  o.Client,
+		Log:     o.Log,
+		DClient: o.DClient,
+		Scheme:  o.Scheme,
+	}
 }
