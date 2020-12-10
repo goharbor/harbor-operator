d4a175397a664ae1e1b6a79db1e00dee5f69b15e
soulseen <zhuxiaoyang1996@gmail.com>
2020-12-10 23:52:11 +0800

    feat(storage):update s3 storage health checker
    Signed-off-by: soulseen <zhuxiaoyang1996@gmail.com>

--- pkg/cluster/controllers/storage/health.go
+++ pkg/cluster/controllers/storage/health.go
@@ -3,11 +3,22 @@ package storage
 import (
 	"context"
 	"fmt"
+	"net/http"
+	"time"
+
 	goharborv1 "github.com/goharbor/harbor-operator/apis/goharbor.io/v1alpha2"
 	"github.com/goharbor/harbor-operator/pkg/cluster/lcm"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
+// HealthChecker for doing health checking for storage.
 type HealthChecker struct{}
+
+var _ lcm.HealthChecker = &HealthChecker{}
 
 func (c *HealthChecker) CheckHealth(ctx context.Context, svc *lcm.ServiceConfig, options ...lcm.Option) (*lcm.CheckResponse, error) {
 
@@ -19,9 +30,9 @@ func (c *HealthChecker) CheckHealth(ctx context.Context, svc *lcm.ServiceConfig,
 
 	switch checkOpts.StorageDriver {
 	case goharborv1.S3DriverName:
-		return S3StorageHealthCheck(ctx, svc)
+		return S3StorageHealthCheck(ctx, svc, checkOpts)
 	case goharborv1.SwiftDriverName:
-		return SwiftStorageHealthCheck(ctx, svc)
+		return SwiftStorageHealthCheck(ctx, svc, checkOpts)
 	case goharborv1.FileSystemDriverName:
 		return &lcm.CheckResponse{
 			Message: "skiped check",
@@ -32,14 +43,46 @@ func (c *HealthChecker) CheckHealth(ctx context.Context, svc *lcm.ServiceConfig,
 	}
 }
 
-func S3StorageHealthCheck(ctx context.Context, svc *lcm.ServiceConfig) (*lcm.CheckResponse, error) {
-	resp := &lcm.CheckResponse{}
+func S3StorageHealthCheck(ctx context.Context, svc *lcm.ServiceConfig, options *lcm.CheckOptions) (*lcm.CheckResponse, error) {
+	checkRes := &lcm.CheckResponse{}
+	edp := svc.Endpoint.Host + string(svc.Endpoint.Port)
+	bucket := &s3.HeadBucketInput{
+		Bucket: aws.String(options.BucketName),
+	}
 
-	return resp, nil
+	// Configure to use s3 Server, also can used for MinIO server
+	s3Config := &aws.Config{
+		Region:           aws.String(options.S3Region),
+		Endpoint:         aws.String(edp),
+		Credentials:      credentials.NewStaticCredentials(svc.Credentials.AccessKey, svc.Credentials.AccessSecret, ""),
+		DisableSSL:       aws.Bool(options.SSLMode),
+		S3ForcePathStyle: aws.Bool(true),
+		HTTPClient:       &http.Client{Timeout: 10 * time.Second,},
+		MaxRetries:       aws.Int(5),
+	}
+
+	newSession, err := session.NewSession(s3Config)
+	if err != nil {
+		return nil, err
+	}
+	s3Client := s3.New(newSession)
+
+	// check if the Bucket exists, If the condition is not met within the max attempt window, an error will
+	// be returned.
+	err = s3Client.WaitUntilBucketExists(bucket)
+	if err != nil {
+		return nil, err
+	}
+
+	checkRes.Status = lcm.Healthy
+	return checkRes, nil
 }
 
-func SwiftStorageHealthCheck(ctx context.Context, svc *lcm.ServiceConfig) (*lcm.CheckResponse, error) {
-	resp := &lcm.CheckResponse{}
+// TODO soulseen
+func SwiftStorageHealthCheck(ctx context.Context, svc *lcm.ServiceConfig, options *lcm.CheckOptions) (*lcm.CheckResponse, error) {
+	resp := &lcm.CheckResponse{
+		Status: lcm.Healthy,
+	}
 
 	return resp, nil
 }
