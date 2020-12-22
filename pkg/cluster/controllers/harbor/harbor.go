a1f30298e09baade9a1af6091b2cefee15b6897a
wangcanfeng <wangcanfeng@corp.netease.com>
2020-12-22 10:35:50 +0800

    feat(harborcluster): update harborcluster cr status
    Signed-off-by: wangcanfeng <wangcanfeng@corp.netease.com>

--- pkg/cluster/controllers/harbor/harbor.go
+++ pkg/cluster/controllers/harbor/harbor.go
@@ -5,19 +5,18 @@ import (
 	"fmt"
 	"sync"
 
-	"github.com/goharbor/harbor-operator/apis/meta/v1alpha1"
-	corev1 "k8s.io/api/core/v1"
-	"sigs.k8s.io/kustomize/kstatus/status"
-
 	"github.com/go-logr/logr"
 	"github.com/goharbor/harbor-operator/apis/goharbor.io/v1alpha2"
+	"github.com/goharbor/harbor-operator/apis/meta/v1alpha1"
 	"github.com/goharbor/harbor-operator/pkg/cluster/lcm"
 	"github.com/goharbor/harbor-operator/pkg/k8s"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/equality"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/kustomize/kstatus/status"
 )
 
 type Controller struct {
