125a567ac4c28d10de00a23bbadb88032b59b09d
Simon Guyennet <simon.guyennet@corp.ovh.com>
2021-01-27 11:56:47 +0100

    fix(harbor class) Close #399
    Signed-off-by: Simon Guyennet <simon.guyennet@corp.ovh.com>

--- pkg/controller/helpers.go
+++ pkg/controller/helpers.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"fmt"
 	"strings"
+
+	"github.com/goharbor/harbor-operator/pkg/config"
+	"github.com/ovh/configstore"
+	"github.com/pkg/errors"
 )
 
 func (c *Controller) NormalizeName(ctx context.Context, name string, suffixes ...string) string {
@@ -14,4 +18,22 @@ func (c *Controller) NormalizeName(ctx context.Context, name string, suffixes ..
 	}
 
 	return name
+}
+
+func (c *Controller) GetClassName(ctx context.Context) (string, error) {
+	className := ""
+
+	configItem, err := configstore.Filter().Store(c.ConfigStore).Slice(config.HarborClassKey).GetFirstItem()
+	if err != nil {
+		if !config.IsNotFound(err, config.HarborClassKey) {
+			return "", errors.Wrap(err, "cannot get config template path")
+		}
+	} else {
+		className, err = configItem.Value()
+		if err != nil {
+			return "", errors.Wrap(err, "invalid config template path")
+		}
+	}
+
+	return className, nil
 }
