241b44b95f95096e90f54c9e4e4d4a34a3162115
soulseen <zhuxiaoyang1996@gmail.com>
2021-02-08 22:30:43 +0800

    fix(storage): fix minio error tls
    Signed-off-by: soulseen <zhuxiaoyang1996@gmail.com>

--- pkg/cluster/controllers/storage/provision.go
+++ pkg/cluster/controllers/storage/provision.go
@@ -245,13 +245,6 @@ func (m *MinIOController) generateMinIOCR(ctx context.Context, harborcluster *go
 		return nil, err
 	}
 
-	externalCertSecret := &minio.LocalCertificateReference{}
-
-	if m.HarborCluster.Spec.InClusterStorage.MinIOSpec.Redirect.Expose != nil && m.HarborCluster.Spec.InClusterStorage.MinIOSpec.Redirect.Expose.TLS != nil {
-		externalCertSecret.Name = m.HarborCluster.Spec.InClusterStorage.MinIOSpec.Redirect.Expose.TLS.CertificateRef
-		externalCertSecret.Type = "kubernetes.io/tls"
-	}
-
 	return &minio.Tenant{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       minio.MinIOCRDResourceKind,
@@ -276,7 +269,6 @@ func (m *MinIOController) generateMinIOCR(ctx context.Context, harborcluster *go
 				Labels:      m.getLabels(),
 				Annotations: m.generateAnnotations(),
 			},
-			ExternalCertSecret: externalCertSecret,
 			ServiceName:        m.getServiceName(),
 			Image:              image,
 			ImagePullPolicy:    m.getImagePullPolicy(ctx, harborcluster),
