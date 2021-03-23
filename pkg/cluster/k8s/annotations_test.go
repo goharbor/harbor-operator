94ee4ddf261faaf0e0ae3077e27a8fa7299642ed
wangcanfeng <wangcanfeng@corp.netease.com>
2021-03-23 14:42:07 +0800

    fix(harborcluster): fix go-lint
    Signed-off-by: wangcanfeng <wangcanfeng@corp.netease.com>

--- pkg/cluster/k8s/annotations_test.go
+++ pkg/cluster/k8s/annotations_test.go
@@ -3,7 +3,7 @@ package k8s_test
 import (
 	"testing"
 
-	goharborv1alpha2 "github.com/goharbor/harbor-operator/apis/goharbor.io/v1alpha2"
+	goharborv1 "github.com/goharbor/harbor-operator/apis/goharbor.io/v1alpha3"
 	"github.com/goharbor/harbor-operator/pkg/cluster/k8s"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -22,21 +22,21 @@ func TestHashEquals(t *testing.T) {
 		{
 			name: "TestHashEquals_1",
 			args: args{
-				o1: &goharborv1alpha2.Harbor{
+				o1: &goharborv1.Harbor{
 					ObjectMeta: metav1.ObjectMeta{
 						Annotations: map[string]string{
 							k8s.HarborClusterLastAppliedHash: "1",
 						},
 					},
-					Spec: goharborv1alpha2.HarborSpec{},
+					Spec: goharborv1.HarborSpec{},
 				},
-				o2: &goharborv1alpha2.Harbor{
+				o2: &goharborv1.Harbor{
 					ObjectMeta: metav1.ObjectMeta{
 						Annotations: map[string]string{
 							k8s.HarborClusterLastAppliedHash: "1",
 						},
 					},
-					Spec: goharborv1alpha2.HarborSpec{},
+					Spec: goharborv1.HarborSpec{},
 				},
 			},
 			want: true,
@@ -45,13 +45,13 @@ func TestHashEquals(t *testing.T) {
 			name: "TestHashEquals_2",
 			args: args{
 				o1: nil,
-				o2: &goharborv1alpha2.Harbor{
+				o2: &goharborv1.Harbor{
 					ObjectMeta: metav1.ObjectMeta{
 						Annotations: map[string]string{
 							k8s.HarborClusterLastAppliedHash: "1",
 						},
 					},
-					Spec: goharborv1alpha2.HarborSpec{},
+					Spec: goharborv1.HarborSpec{},
 				},
 			},
 			want: false,
@@ -67,21 +67,21 @@ func TestHashEquals(t *testing.T) {
 		{
 			name: "TestHashEquals_4",
 			args: args{
-				o1: &goharborv1alpha2.Harbor{
+				o1: &goharborv1.Harbor{
 					ObjectMeta: metav1.ObjectMeta{
 						Annotations: map[string]string{
 							k8s.HarborClusterLastAppliedHash: "1",
 						},
 					},
-					Spec: goharborv1alpha2.HarborSpec{},
+					Spec: goharborv1.HarborSpec{},
 				},
-				o2: &goharborv1alpha2.Harbor{
+				o2: &goharborv1.Harbor{
 					ObjectMeta: metav1.ObjectMeta{
 						Annotations: map[string]string{
 							k8s.HarborClusterLastAppliedHash: "2",
 						},
 					},
-					Spec: goharborv1alpha2.HarborSpec{},
+					Spec: goharborv1.HarborSpec{},
 				},
 			},
 			want: false,
