76550709884a5ee20d64f2d77b67d290730fcfff
wangcanfeng <wangcanfeng@corp.netease.com>
2021-04-06 20:03:32 +0800

    fix(harborcluster): revert unnecessary changes
    Signed-off-by: wangcanfeng <wangcanfeng@corp.netease.com>

--- pkg/resources/checksum/checksum_test.go
+++ pkg/resources/checksum/checksum_test.go
@@ -94,7 +94,6 @@ var _ = Describe("Checksum", func() {
 			depManager.AddAnnotations(resource)
 
 			owner.SetGeneration(100)
-			owner.Spec.Replicas = func(i int32) *int32 { return &i }(2)
 
 			changed := depManager.ChangedFor(ctx, resource)
 			Expect(changed).To(Equal(true))
