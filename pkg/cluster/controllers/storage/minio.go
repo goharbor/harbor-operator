72e635cfc6261f9c089065c75a51a16b4592a29a
soulseen <zhuxiaoyang1996@gmail.com>
2021-04-26 20:21:45 +0800

    fix(storgae): update ci
    Signed-off-by: soulseen <zhuxiaoyang1996@gmail.com>

--- pkg/cluster/controllers/storage/minio.go
+++ pkg/cluster/controllers/storage/minio.go
@@ -141,9 +141,17 @@ func (m *MinIOController) createInitJob(ctx context.Context, harborcluster *goha
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      m.getMinIONamespacedName(harborcluster).Name,
 			Namespace: m.getMinIONamespacedName(harborcluster).Namespace,
+			Labels: map[string]string{
+				"job-type":"minio-init",
+			},
 		},
 		Spec: batchv1.JobSpec{
 			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"job-type":"minio-init",
+					},
+				},
 				Spec: corev1.PodSpec{
 					RestartPolicy: corev1.RestartPolicyOnFailure,
 					Containers: []corev1.Container{
